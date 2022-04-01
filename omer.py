a02f9bf3c520e834dc178e584d2b39b50ef061ea
code <1994omerz@gmail.com>
2022-04-01 23:22:24 +0300

    this is my first code in python

---
+++ omer.py
@@ -0,0 +1,18 @@
+a=14
+b=12.67
+c="omer"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+age=input("Enter Your Age : ")
+name=input("Enter your name: ")
+mail=input("Enter your mail: ")
+print("\nFull name " + name + " \nYour Age Is: " + str(age) + "\nYour mail: " +mail)
+
